9a28cef74d3e0fad68f7f143378a09b4772ec0cf
madalin-fr <madalin-gabriel.frincu@my.fmi.unibuc.ro>
2023-06-21 20:19:55 +0300

    minor changes

--- middleware/auth-guest.js
+++
@@ -1,7 +0,0 @@
-export default function ({ app, redirect }) {
-  // Check if the user is authenticated
-  if (app.$auth.loggedIn) {
-    // If the user is authenticated, redirect them to the dashboard
-    return redirect('/furniture-list');
-  }
-}
